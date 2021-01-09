67db0bb7f6dd2a22813b692c880d2a62d5d318fc
Ouwzhong <41533813+Ouwzhong@users.noreply.github.com>
2021-01-09 23:17:49 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -3,45 +3,45 @@
  */
 #include <stdio.h>
 #include <time.h>
-
 typedef signed char BYTE;
 typedef signed short WORD;
 
 const char iFileName[] = "data.yuv"; 
 const char oFileName[] = "result.yuv"; 
 
+extern cregister volatile unsigned int TSCL;
+extern cregister volatile unsigned int TSCH;
+
 BYTE** initMat(int width, int height, FILE *piFile); 
 WORD** H_transform(int height, int width, BYTE** X);
-WORD** H_dotp(BYTE** N);
-WORD** H_dotp_inv(WORD** N);
 void saveMat(int height, int width, WORD** Y, FILE *poFile);
 void show_mat(int height, int width, WORD** Mat);
 
-int main(void) {
+clock_t clock();
 
+int main(void) {
+	
 	const int width = 352;
 	const int height = 288;
 
 	FILE* piFile = fopen(iFileName, "rb");	//pointer input file
 	FILE* poFile = fopen(oFileName, "wb");	//pointer output file
-	char buffer [80];
-	const time_t  start = time(0);
-	struct tm * timeinfo = localtime (&start);
-	strftime (buffer,80,"%c.",timeinfo);
-	printf("[main.c  %s] Start to read file.\n",buffer);
-	int i;
-	for  (i = 0; i<height/4; i++){
-	BYTE **X = initMat(4, width, piFile);
-	WORD **Y = H_transform(4, width, X);
-	saveMat(4, width, Y, poFile);
-	}
-	const time_t  t1 = time(0);
-	timeinfo = localtime (&t1);
-	strftime (buffer,80,"%c.",timeinfo);
-	printf("[main.c  %s] Done. time: %d\n",buffer,t1-start);
-	// BYTE **X = initMat(height, width, piFile);
-	// BYTE **Y = H_transform(height, width, X);
-	// saveMat(height, width, Y, poFile);
+
+	// int i;
+	// for  (i = 0; i<height/4; i++){
+	// BYTE **X = initMat(4, width, piFile);
+	// WORD **Y = H_transform(4, width, X);
+	// saveMat(4, width, Y, poFile);
+	// }
+	
+	TSCL = 1;
+	BYTE **X = initMat(height, width, piFile);
+	clock_t start_t = clock();		//start counting CPU cycles
+	WORD **Y = H_transform(height, width, X);
+	clock_t end_t = clock();		//stop to count CPU cycles
+	saveMat(height, width, Y, poFile);
+
+	printf("Transform took %d cycles.",end_t - start_t);
 	
 	fclose(piFile);
     fclose(poFile);
@@ -74,138 +74,72 @@ void saveMat(int height, int width, WORD** Y, FILE *poFile){
 WORD** H_transform(int height, int width, BYTE** X){
 	int y, ymax = height / 4;
 	int x, xmax = width / 4;
-	BYTE* X0 = (BYTE *)calloc(4,sizeof(BYTE));
-	BYTE* X1 = (BYTE *)calloc(4,sizeof(BYTE));
-	BYTE* X2 = (BYTE *)calloc(4,sizeof(BYTE));
-	BYTE* X3 = (BYTE *)calloc(4,sizeof(BYTE));
-	BYTE** Xpart = (BYTE **)calloc(4,sizeof(BYTE*));
-	Xpart[0] = X0;
-	Xpart[1] = X1;
-	Xpart[2] = X2;
-	Xpart[3] = X3;
 
-	WORD** Ypart;
 	WORD **Y = (WORD **)calloc(height,sizeof(WORD*));
 	for (y=0;y<height;y++){
 		Y[y] = (WORD *)calloc(width,sizeof(WORD));
 	}
+	WORD a00,a01,a02,a03,a10,a11,a12,a13,a20,a21,a22,a23,a30,a31,a32,a33;	//temp result
 
 	int true_x, true_y;
 	for (y=0; y<ymax; y++){
 		for (x=0; x<xmax; x++){
 			true_x = 4*x;
 			true_y = 4*y;
-			Xpart[0][0] = X[true_y][true_x];
-			Xpart[0][1] = X[true_y][true_x+1];
-			Xpart[0][2] = X[true_y][true_x+2];
-			Xpart[0][3] = X[true_y][true_x+3];
-
-			Xpart[1][0] = X[true_y+1][true_x];
-			Xpart[1][1] = X[true_y+1][true_x+1];
-			Xpart[1][2] = X[true_y+1][true_x+2];
-			Xpart[1][3] = X[true_y+1][true_x+3];
-
-			Xpart[2][0] = X[true_y+2][true_x];
-			Xpart[2][1] = X[true_y+2][true_x+1];
-			Xpart[2][2] = X[true_y+2][true_x+2];
-			Xpart[2][3] = X[true_y+2][true_x+3];
-
-			Xpart[3][0] = X[true_y+3][true_x];
-			Xpart[3][1] = X[true_y+3][true_x+1];
-			Xpart[3][2] = X[true_y+3][true_x+2];
-			Xpart[3][3] = X[true_y+3][true_x+3];
 			
-			Ypart = H_dotp_inv(H_dotp(Xpart));
-			// show_mat(4, 4, Ypart);
+			// A = H*X
+			a00 = *(*(X+true_y)+true_x) + *(*(X+true_y+1)+true_x) + *(*(X+true_y+2)+true_x) + *(*(X+true_y+3)+true_x);
+			a01 = *(*(X+true_y)+true_x+1) + *(*(X+true_y+1)+true_x+1) + *(*(X+true_y+2)+true_x+1) + *(*(X+true_y+3)+true_x+1);
+			a02 = *(*(X+true_y)+true_x+2) + *(*(X+true_y+1)+true_x+2) + *(*(X+true_y+2)+true_x+2) + *(*(X+true_y+3)+true_x+2);
+			a03 = *(*(X+true_y)+true_x+3) + *(*(X+true_y+1)+true_x+3) + *(*(X+true_y+2)+true_x+3) + *(*(X+true_y+3)+true_x+3);
 
-			Y[true_y][true_x] = Ypart[0][0];
-			Y[true_y][true_x+1] = Ypart[0][1];
-			Y[true_y][true_x+2] = Ypart[0][2];
-			Y[true_y][true_x+3] = Ypart[0][3];
+			a10 = 2**(*(X+true_y)+true_x) + *(*(X+true_y+1)+true_x) - *(*(X+true_y+2)+true_x) - 2**(*(X+true_y+3)+true_x);
+			a11 = 2**(*(X+true_y)+true_x+1) + *(*(X+true_y+1)+true_x+1) - *(*(X+true_y+2)+true_x+1) - 2**(*(X+true_y+3)+true_x+1);
+			a12 = 2**(*(X+true_y)+true_x+2) + *(*(X+true_y+1)+true_x+2) - *(*(X+true_y+2)+true_x+2) - 2**(*(X+true_y+3)+true_x+2);
+			a13 = 2**(*(X+true_y)+true_x+3) + *(*(X+true_y+1)+true_x+3) - *(*(X+true_y+2)+true_x+3) - 2**(*(X+true_y+3)+true_x+3);
 
-			Y[true_y+1][true_x] = Ypart[1][0];
-			Y[true_y+1][true_x+1] = Ypart[1][1];
-			Y[true_y+1][true_x+2] = Ypart[1][2];
-			Y[true_y+1][true_x+3] = Ypart[1][3];
+			a20 = *(*(X+true_y)+true_x) - *(*(X+true_y+1)+true_x) - *(*(X+true_y+2)+true_x) + *(*(X+true_y+3)+true_x);
+			a21 = *(*(X+true_y)+true_x+1) - *(*(X+true_y+1)+true_x+1) - *(*(X+true_y+2)+true_x+1) + *(*(X+true_y+3)+true_x+1);
+			a22 = *(*(X+true_y)+true_x+2) - *(*(X+true_y+1)+true_x+2) - *(*(X+true_y+2)+true_x+2) + *(*(X+true_y+3)+true_x+2);
+			a23 = *(*(X+true_y)+true_x+3) - *(*(X+true_y+1)+true_x+3) - *(*(X+true_y+2)+true_x+3) + *(*(X+true_y+3)+true_x+3);
 
-			Y[true_y+2][true_x] = Ypart[2][0];
-			Y[true_y+2][true_x+1] = Ypart[2][1];
-			Y[true_y+2][true_x+2] = Ypart[2][2];
-			Y[true_y+2][true_x+3] = Ypart[2][3];
+			a30 = *(*(X+true_y)+true_x) - 2**(*(X+true_y+1)+true_x) + 2**(*(X+true_y+2)+true_x) - *(*(X+true_y+3)+true_x);
+			a31 = *(*(X+true_y)+true_x+1) - 2**(*(X+true_y+1)+true_x+1) + 2**(*(X+true_y+2)+true_x+1) - *(*(X+true_y+3)+true_x+1);
+			a32 = *(*(X+true_y)+true_x+2) - 2**(*(X+true_y+1)+true_x+2) + 2**(*(X+true_y+2)+true_x+2) - *(*(X+true_y+3)+true_x+2);
+			a33 = *(*(X+true_y)+true_x+3) - 2**(*(X+true_y+1)+true_x+3) + 2**(*(X+true_y+2)+true_x+3) - *(*(X+true_y+3)+true_x+3);
 
-			Y[true_y+3][true_x] = Ypart[3][0];
-			Y[true_y+3][true_x+1] = Ypart[3][1];
-			Y[true_y+3][true_x+2] = Ypart[3][2];
-			Y[true_y+3][true_x+3] = Ypart[3][3];
+
+			// Y = A*H^T
+			*(*(Y+true_y)+true_x) = a00 + a01 + a02 + a03;
+			*(*(Y+true_y+1)+true_x) = a10 + a11 + a12 + a13;
+			*(*(Y+true_y+2)+true_x)= a20 + a21 + a22 + a23;
+			*(*(Y+true_y+3)+true_x) = a30 + a31 + a32 + a33;
+
+			*(*(Y+true_y)+true_x+1) = 2*a00 + a01 - a02 - 2*a03;
+			*(*(Y+true_y+1)+true_x+1) = 2*a10 + a11 - a12 - 2*a13;
+			*(*(Y+true_y+2)+true_x+1) = 2*a20 + a21 - a22 - 2*a23;
+			*(*(Y+true_y+3)+true_x+1) = 2*a30 + a31 - a32 - 2*a33;
+
+			*(*(Y+true_y)+true_x+2) = a00 - a01 - a02 + a03;
+			*(*(Y+true_y+1)+true_x+2) = a10 - a11 - a12 + a13;
+			*(*(Y+true_y+2)+true_x+2) = a20 - a21 - a22 + a23;
+			*(*(Y+true_y+3)+true_x+2) = a30 - a31 - a32 + a33;
+
+			*(*(Y+true_y)+true_x+3) = a00 - 2*a01 + 2*a02 - a03;
+			*(*(Y+true_y+1)+true_x+3) = a10 - 2*a11 + 2*a12 - a13;
+			*(*(Y+true_y+2)+true_x+3) = a20 - 2*a21 + 2*a22 - a23;
+			*(*(Y+true_y+3)+true_x+3) = a30 - 2*a31 + 2*a32 - a33;
+			
 		}
 	}
 	return Y;
 }
 
-WORD** H_dotp(BYTE** N){
-	WORD* Y0 = (WORD *)calloc(4,sizeof(WORD));
-	WORD* Y1 = (WORD *)calloc(4,sizeof(WORD));
-	WORD* Y2 = (WORD *)calloc(4,sizeof(WORD));
-	WORD* Y3 = (WORD *)calloc(4,sizeof(WORD));
-	WORD** Y = (WORD **)calloc(4,sizeof(WORD*));
-	Y[0] = Y0;
-	Y[1] = Y1;
-	Y[2] = Y2;
-	Y[3] = Y3;
-
-	Y[0][0] = N[0][0] + N[1][0] + N[2][0] + N[3][0];
-	Y[0][1] = N[0][1] + N[1][1] + N[2][1] + N[3][1];
-	Y[0][2] = N[0][2] + N[1][2] + N[2][2] + N[3][2];
-	Y[0][3] = N[0][3] + N[1][3] + N[2][3] + N[3][3];
-
-	Y[1][0] = 2*N[0][0] + N[1][0] - N[2][0] - 2*N[3][0];
-	Y[1][1] = 2*N[0][1] + N[1][1] - N[2][1] - 2*N[3][1];
-	Y[1][2] = 2*N[0][2] + N[1][2] - N[2][2] - 2*N[3][2];
-	Y[1][3] = 2*N[0][3] + N[1][3] - N[2][3] - 2*N[3][3];
-
-	Y[2][0] = N[0][0] - N[1][0] - N[2][0] + N[3][0];
-	Y[2][1] = N[0][1] - N[1][1] - N[2][1] + N[3][1];
-	Y[2][2] = N[0][2] - N[1][2] - N[2][2] + N[3][2];
-	Y[2][3] = N[0][3] - N[1][3] - N[2][3] + N[3][3];
-
-	Y[3][0] = N[0][0] - 2*N[1][0] + 2*N[2][0] - N[3][0];
-	Y[3][1] = N[0][1] - 2*N[1][1] + 2*N[2][1] - N[3][1];
-	Y[3][2] = N[0][2] - 2*N[1][2] + 2*N[2][2] - N[3][2];
-	Y[3][3] = N[0][3] - 2*N[1][3] + 2*N[2][3] - N[3][3];
-	return Y;
-}
-
-WORD** H_dotp_inv(WORD** N){
-	WORD* Y0 = (WORD *)calloc(4,sizeof(WORD));
-	WORD* Y1 = (WORD *)calloc(4,sizeof(WORD));
-	WORD* Y2 = (WORD *)calloc(4,sizeof(WORD));
-	WORD* Y3 = (WORD *)calloc(4,sizeof(WORD));
-	WORD** Y = (WORD **)calloc(4,sizeof(WORD*));
-	Y[0] = Y0;
-	Y[1] = Y1;
-	Y[2] = Y2;
-	Y[3] = Y3;
-
-	Y[0][0] = N[0][0] + N[0][1] + N[0][2] + N[0][3];
-	Y[1][0] = N[1][0] + N[1][1] + N[1][2] + N[1][3];
-	Y[2][0] = N[2][0] + N[2][1] + N[2][2] + N[2][3];
-	Y[3][0] = N[3][0] + N[3][1] + N[3][2] + N[3][3];
-
-	Y[0][1] = 2*N[0][0] + N[0][1] - N[0][2] - 2*N[0][3];
-	Y[1][1] = 2*N[1][0] + N[1][1] - N[1][2] - 2*N[1][3];
-	Y[2][1] = 2*N[2][0] + N[2][1] - N[2][2] - 2*N[2][3];
-	Y[3][1] = 2*N[3][0] + N[3][1] - N[3][2] - 2*N[3][3];
-
-	Y[0][2] = N[0][0] - N[0][1] - N[0][2] + N[0][3];
-	Y[1][2] = N[1][0] - N[1][1] - N[1][2] + N[1][3];
-	Y[2][2] = N[2][0] - N[2][1] - N[2][2] + N[2][3];
-	Y[3][2] = N[3][0] - N[3][1] - N[3][2] + N[3][3];
-
-	Y[0][3] = N[0][0] - 2*N[0][1] + 2*N[0][2] - N[0][3];
-	Y[1][3] = N[1][0] - 2*N[1][1] + 2*N[1][2] - N[1][3];
-	Y[2][3] = N[2][0] - 2*N[2][1] + 2*N[2][2] - N[2][3];
-	Y[3][3] = N[3][0] - 2*N[3][1] + 2*N[3][2] - N[3][3];
-	return Y;
+clock_t clock(){
+    unsigned int low = TSCL;
+    unsigned int high = TSCH;
+    if (high) return (clock_t) -1;
+    return low;
 }
 
 void show_mat(int height, int width, WORD** Mat){
